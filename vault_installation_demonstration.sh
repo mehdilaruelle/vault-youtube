be24c606692fc9de27859860d080930d846631ff
mehdilaruelle <mehdi.laruelle@revolve.team>
2021-03-14 20:35:25 +0100

    Add comment for vault installation commands

--- vault_installation_demonstration.sh
+++ vault_installation_demonstration.sh
@@ -1,20 +1,30 @@
+#! /bin/bash
+
+###########################
+# Based on Amazon Linux 2 #
+###########################
+
+# Docker installation
 sudo amazon-linux-extras install -y docker
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 
-docker run --cap-add=IPC_LOCK -d -p 8200:8200 --name=dev-vault vault 
+# Vault dev mode with Docker
+docker run --cap-add=IPC_LOCK -d -p 8200:8200 --name=dev-vault vault
 docker ps
 docker logs dev-vault
 curl http://127.0.0.1:8200/v1/sys/health
 docker stop dev-vault
 docker rm dev-vault
 
+# Vault installation with yum repository
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install vault
 vault -version
 
+# Vault dev mode with Vault binary
 vault server -dev -dev-root-token-id=root &
 export VAULT_ADDR='http://127.0.0.1:8200'
 vault status
@@ -25,12 +35,14 @@ CTRL + C
 unset VAULT_ADDR
 unset VAULT_TOKEN
 
+# Vault Startup
 cat /etc/vault.d/vault.hcl
 sudo vault server -config=/etc/vault.d/vault.hcl &
 export VAULT_ADDR='https://127.0.0.1:8200'
 export VAULT_SKIP_VERIFY='true'
 vault status
 
+# Vault initialization
 vault operator init
 vault status
 vault operator unseal
@@ -39,6 +51,7 @@ vault status
 vault login
 vault token lookup
 
+# Vault check storage backend encryption
 fg
 CTRL + C
 sudo vault server -config=/etc/vault.d/vault.hcl &
